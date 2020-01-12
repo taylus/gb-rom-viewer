b93b115fc3f3311fdf42378d99421d25f0052855
taylus <brandon@legacy.codes>
2020-01-11 21:44:03 -0600

    Read a ROM and produce a sample image based on its size

--- GBRomViewer/Program.cs
+++ GBRomViewer/Program.cs
@@ -8,10 +8,16 @@ namespace GBRomViewer
     public class Program
     {
         private const string outputFileName = "test.png";
+        private const int tileWidthInPixels = 8;
+        private const int outputImageWidthInTiles = 64;
+        private const int outputImageWidthInPixels = outputImageWidthInTiles * tileWidthInPixels;
 
         public static void Main()
         {
-            using (var image = new Image<Rgba32>(256, 256))
+            var rom = File.ReadAllBytes(@"C:\roms\gb\Pokemon - Blue Version (USA, Europe) (SGB Enhanced).gb");
+            int outputImageHeightInPixels = GetOutputImageHeight(outputImageWidthInTiles, rom);
+
+            using (var image = new Image<Rgba32>(outputImageWidthInPixels, outputImageHeightInPixels))
             using (var stream = new FileStream(outputFileName, FileMode.OpenOrCreate))
             {
                 for (int x = 0; x < image.Width; x++)
@@ -26,6 +32,16 @@ namespace GBRomViewer
             }
 
             Process.Start(new ProcessStartInfo() { FileName = outputFileName, UseShellExecute = true });
+        }
+
+        private static int GetOutputImageHeight(int outputImageWidthInTiles, byte[] rom)
+        {
+            //interpreting bytes as 2bpp pixel data:
+            //each consecutive pair of bytes is one 8 pixel row of data in a tile
+            //16 bytes make up one tile
+            //given the desired width of the output image (in tiles),
+            //the height of output image in pixels = # of bytes in ROM / (16 bytes per tile * width in tiles)
+            return rom.Length / (16 * outputImageWidthInTiles);
         }
     }
 }

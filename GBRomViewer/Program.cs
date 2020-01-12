ec56d1a1919d6a53c855327848329691c3160c7d
taylus <brandon@legacy.codes>
2020-01-12 15:43:05 -0600

    Set up command-line arg handling and update README

--- GBRomViewer/Program.cs
+++ GBRomViewer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -7,22 +8,22 @@ namespace GBRomViewer
 {
     public class Program
     {
-        //TODO: load from command-line args
-        private const string outputFileName = "output.png";
-        private const int outputImageWidthInTiles = 16;
-        private const int outputImageWidthInPixels = outputImageWidthInTiles * Tile.Width;
-        private const string inputRom = @"C:\roms\gb\Pokemon - Blue Version (USA, Europe) (SGB Enhanced).gb";
+        private static int outputImageWidthInTiles = 16;
 
-        public static void Main()
+        public static void Main(string[] args)
         {
-            var rom = File.ReadAllBytes(inputRom);
+            ParseCommandLineArgs(args, out string outFile, out string inFile);
+            if (inFile == null || outFile == null) return;
+
+            var rom = File.ReadAllBytes(inFile);
             var tiles = Tiles.LoadFrom(rom);
 
             //plot the tiles onto an output image using a given color palette
             var palette = GameBoyColorPalette.Dmg;
+            int outputImageWidthInPixels = outputImageWidthInTiles * Tile.Width;
             int outputImageHeightInPixels = GetOutputImageHeightInTiles(outputImageWidthInTiles, rom) * Tile.Height;
             using (var image = new Image<Rgba32>(outputImageWidthInPixels, outputImageHeightInPixels))
-            using (var stream = new FileStream(outputFileName, FileMode.OpenOrCreate))
+            using (var stream = new FileStream(outFile, FileMode.OpenOrCreate))
             {
                 for (int i = 0; i < tiles.Count; i++)
                 {
@@ -33,13 +34,37 @@ namespace GBRomViewer
                 image.SaveAsPng(stream);
             }
 
-            Process.Start(new ProcessStartInfo() { FileName = outputFileName, UseShellExecute = true });
+            Process.Start(new ProcessStartInfo() { FileName = outFile, UseShellExecute = true });
         }
 
         private static int GetOutputImageHeightInTiles(int outputImageWidthInTiles, byte[] rom)
         {
             int tilesInRom = rom.Length / Tile.BytesPerTile;
             return tilesInRom / outputImageWidthInTiles;
+        }
+
+        private static void ParseCommandLineArgs(string[] args, out string outFile, out string inFile)
+        {
+            if (args.Length == 1)
+            {
+                inFile = args[0];
+                outFile = Path.ChangeExtension(inFile, ".png");
+            }
+            else if (args.Length == 3 && args[0] == "-o")
+            {
+                outFile = args[1];
+                inFile = args[2];
+            }
+            else if (args.Length == 5 && args[0] == "-o" && args[2] == "-tw" && int.TryParse(args[3], out outputImageWidthInTiles))
+            {
+                outFile = args[1];
+                inFile = args[4];
+            }
+            else
+            {
+                outFile = inFile = null;
+                Console.WriteLine("Usage: gbromviewer [-o output.png] [-tw 16] rom.gb");
+            }
         }
     }
 }
